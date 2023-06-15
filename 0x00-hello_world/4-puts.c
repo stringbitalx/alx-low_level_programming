8e2e70d7aaa1ae3eec227f471b39ccd87f844a67
stringbitalx <stringbitalx@gmail.com>
2023-06-16 02:08:35 +0300

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Alwaysa 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
