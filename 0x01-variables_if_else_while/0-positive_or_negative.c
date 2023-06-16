09ac156362aed35c02b1f77a621b57c179e0be89
stringbitalx <stringbitalx@gmail.com>
2023-06-16 16:11:17 -0700

    edited comments

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,9 @@
 /**
  * main - Determine if a number is positive, negative or zero.
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
