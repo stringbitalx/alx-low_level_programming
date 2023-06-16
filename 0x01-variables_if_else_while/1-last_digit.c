63b141dd6caa1a349ea0c93fbaa61507b768b7f2
stringbitalx <stringbitalx@gmail.com>
2023-06-16 16:39:24 -0700

    used & with AND in printf

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if a number is greater than 5, equal to 0 or less than 6
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 5)
+	{
+		printf("Last digit of \&\n is %d and is greater than 5/n", n);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of \&\n is %d and is 0/n", n);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of \&\n is %d and is less than 6 and not 0/n", n)
+	}
+	return (0);
+}
