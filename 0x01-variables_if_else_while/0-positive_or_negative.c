6b1f80ca59cff50fcb50aa64455fc4eaaa7f3992
stringbitalx <stringbitalx@gmail.com>
2023-06-16 16:02:08 -0700

    corrected compilation error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,6 +22,7 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
@@ -29,7 +30,6 @@ int main(void)
 	else
 	{
 		printf("%d is negative\n", n);
-	}
 	}
 	return (0);
 }
