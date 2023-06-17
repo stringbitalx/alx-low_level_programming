306e87cef24853a9067771add7c1ac60c154bb99
stringbitalx <stringbitalx@gmail.com>
2023-06-16 19:03:58 -0700

    putchar using hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main  - printhexadecimal numbers in lowercas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 61; a < 7A; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
