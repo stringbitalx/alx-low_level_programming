0d0cfa817d25ccd0c98762179eec20a5f6658408
stringbitalx <stringbitalx@gmail.com>
2023-06-17 13:11:34 -0700

    final update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -8,6 +8,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	returni (0);
 
 }
