48b5b8cf3eb72ee84288a0c00e4cfe1cee15454c
stringbitalx <stringbitalx@gmail.com>
2023-06-16 18:28:12 -0700

    putchar using decimal

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	
+	putchar('\n');
+	return (0);
+}
