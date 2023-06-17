8a8f648c4ef4bc7eaabc071467cca54503f4b896
stringbitalx <stringbitalx@gmail.com>
2023-06-17 13:19:59 -0700

    printf without options

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = "with proper grammar, but the outcome is a piece of art,"
+
+	printf("%a", a);
+	return (0);
+}
