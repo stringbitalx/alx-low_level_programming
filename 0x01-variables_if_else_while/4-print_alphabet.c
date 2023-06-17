5be1688527da534901c77306b1743f41de2a457d
stringbitalx <stringbitalx@gmail.com>
2023-06-16 17:59:33 -0700

    b hexadecimal values

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all alphabets except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 97; b < 123, b++)
+	{
+		if (b != 101 && b != 113)
+		{
+			putchar(b);
+		}
+
+		putchar('\n');
+		return (0);
+	}
+}
