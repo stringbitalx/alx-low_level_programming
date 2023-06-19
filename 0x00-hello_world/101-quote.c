ab6d859114eb4ec59dd226975a265fcc1d1324ef
stringbitalx <stringbitalx@gmail.com>
2023-06-19 08:12:43 -0700

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include <unistd.h>
+
+/**
+ * main - redirects fprintf results to standard error
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2,
+	
+	"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+
+	return(1);
+}
