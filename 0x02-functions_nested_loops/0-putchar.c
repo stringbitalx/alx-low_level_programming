ea3f5c41ebe3529eed73e60f460cb7731da81082
stringbitalx <stringbitalx@gmail.com>
2023-06-21 10:03:34 -0700

    used putchar command

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,22 @@
 /**
  * main - prints _putchar
  *
+ *is wwritten
+
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	write(1,
-	"_putchar\n", 8);
-
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchat('\n');
+	
 	return (0);
 }
