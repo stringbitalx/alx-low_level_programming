dff9d4a26db276ac6b85c1d25b98198bf0658a5c
stringbitalx <stringbitalx@gmail.com>
2023-06-21 16:32:57 -0700

    included sum variable

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @i: first number
+ * @j: second number
+ *
+ * Return the sum of the two numbers.
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = (i + j);
+
+	return (sum);
+}
