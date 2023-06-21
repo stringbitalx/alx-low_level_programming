bb05448d6bbb67d3175f81596c8fa66a5c304d4d
stringbitalx <stringbitalx@gmail.com>
2023-06-21 13:54:32 -0700

    used _putchar inside 3 if conditionals

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - prints the sign of a number.
+ * @n: The number to be printed.
+ *
+ * Return: 1 if n is greater than zero,
+ * -1 if n is less than zero,
+ * 0 if n is zero.
+ */
+
+int find_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+}
