6d83ffa4c7c466247546d04ee409500449484e67
stringbitalx <stringbitalx@gmail.com>
2023-06-21 12:34:23 -0700

    included putchar command within body of 2 for loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times followed by newline
+ */
+
+void print_alphabet_x10(void)
+{
+	char lower;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for(lower = 'a'; lower <= 'z'; lower++)
+		{
+			_putchar(lower);
+		}
+		
+		_putchar('\n');
+	}
+}
