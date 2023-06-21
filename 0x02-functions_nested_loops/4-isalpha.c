011dc6df759421a2dcb8b40c6b443941521e53a1
stringbitalx <stringbitalx@gmail.com>
2023-06-21 13:30:47 -0700

    final updates

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for alphabetic characters.
+ * _isalpha - checks for alphabetic characters.
  * @c: The character to be checked.
  *
  * Return: 1 if c is alphabet, 0 otherwise.
