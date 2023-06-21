f44c10d61cd0c5a5feae740dee20859c251d1d86
stringbitalx <stringbitalx@gmail.com>
2023-06-21 16:54:53 -0700

    used printf command in if conditional

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98m - prints all natural numbers in range 0-98.
+ * @n: the number to begin counting at n.
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++)
+		printf("%d\n", n);
+	}
+}
