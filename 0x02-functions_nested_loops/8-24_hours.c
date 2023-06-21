860464b9ac6cf935480e6c15f5d1cc056ff74dd8
stringbitalx <stringbitalx@gmail.com>
2023-06-21 16:01:14 -0700

    used ASCII in _putchar function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time in 24 hour format.
+ */
+
+void jack_bauer(void)
+{
+	int hour , min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
