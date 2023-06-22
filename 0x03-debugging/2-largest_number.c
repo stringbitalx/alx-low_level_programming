7e7b8f6eb28a6f18674dffb6770a8fc12543281b
stringbitalx <stringbitalx@gmail.com>
2023-06-22 12:13:14 -0700

    appended else if conditional

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,8 +19,10 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	else if (b > a && b > c)
 		largest = b;
-	else
+	else if (c > a && c > b)
 		largest = c;
+	else
+		largest = -1;
 
 	return (largest);
 }
