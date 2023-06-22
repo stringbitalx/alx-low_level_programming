6564e776bd32d29001ed10d1246e222352d54a1f
stringbitalx <stringbitalx@gmail.com>
2023-06-22 10:43:14 -0700

    final updates

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that prints positive or negative integer
- * Return: 0
+ * main - test that the function positive_or_negative.
+ * Return: Always 0.
  */
 
 int main(void)
 {
-        	int i;
+		int i;
 
-        		i = 0;
-        			positive_or_negative(i);
+			i = 0;
+				positive_or_negative(i);
 
-        			return (0);
+				return (0);
 }
