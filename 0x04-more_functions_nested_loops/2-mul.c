a7007b091f5ac0d1703043d0d9bd0792bff7ec63
stringbitalx <stringbitalx@gmail.com>
2023-06-23 12:51:24 -0700

    used mul function in new 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: digit to be multiplied
+ * @b: digit to be multiplied
+ * Return: 0 success
+ */
+
+int mul(int a, int b)
+{
+	int multiple = a * b;
+
+	return (multiple);
+}
