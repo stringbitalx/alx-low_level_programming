676ff95006cf498294da874ba82adee23159e476
stringbitalx <stringbitalx@gmail.com>
2023-06-23 14:21:59 -0700

    final updates

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,8 @@ void more_numbers(void)
 		for (a = 0; a <= 14; a++)
 		{
 			if (a > 9)
-			{
 				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-			}
-			_putchar(a + '0')
+			_putchar((a % 10) + '0')
 		}
 		_putchar('\n');
 	}
