bc861ace9f2b1057cd627ac311c98db0891fc4e9
stringbitalx <stringbitalx@gmail.com>
2023-06-23 14:14:45 -0700

    created 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+			}
+			_putchar(a + '0')
+		}
+		_putchar('\n');
+	}
+}
