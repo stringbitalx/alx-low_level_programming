f6695155fefc477fddd0fc32c5495d21cf1d06b0
stringbitalx <stringbitalx@gmail.com>
2023-06-23 13:16:04 -0700

    used _putchar function in new 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9 followed by newline
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i
+
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+
+	_putchar("\n")
+	
+	return (0);
+}
