c3b6279c90d2ff5770379f5dea927d4927206d20
stringbitalx <stringbitalx@gmail.com>
2023-06-23 13:27:49 -0700

    final updates

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,13 @@
 
 /**
  * print_numbers - prints numbers 0-9 followed by newline
- * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
 	int i;
 
-		for (i = '0'; i <= '9'; i++)
+		for (i = 0; i <= 9; i++)
 			_putchar(i + '0');
 
 	_putchar("\n");
