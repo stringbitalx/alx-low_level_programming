dfcf0990a588f0fe78ae8c031145ec8cce320791
stringbitalx <stringbitalx@gmail.com>
2023-06-25 04:37:19 -0700

    created 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints "n" times table
+ * @n: size of table
+ * Return: 0 Always
+ */
+
+void print_times_table(int n)
+{
+	int column, row, multiple;
+
+	if (n > 15 || n < 0)
+		return;
+
+	for (column = 0; column <= n; column ++)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			multiple = row * column;
+			_putchars(multiple + '0')
+		}
+
+		_putchar("\n");
+	}
+}
