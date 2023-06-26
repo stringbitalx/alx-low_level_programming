62d89514fd70eac87cc670880848d8b4df1ad723
stringbitalx <stringbitalx@gmail.com>
2023-06-26 14:15:27 -0700

    final updates

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer and updates the value to 98
- * @*n: int pointer used to update variable to 98
+ * @n: int pointer used to update variable to 98
  */
 
 void reset_to_98(int *n)
