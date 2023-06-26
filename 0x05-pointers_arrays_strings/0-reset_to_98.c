d6407bb733f924424121daa00f23be74449cf045
stringbitalx <stringbitalx@gmail.com>
2023-06-26 14:12:50 -0700

    created 0-main.c and 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates the value to 98
+ * @*n: int pointer used to update variable to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
