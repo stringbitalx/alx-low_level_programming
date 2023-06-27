7e67f18e55e4486a02f6771b6ca3a564805e4d08
stringbitalx <stringbitalx@gmail.com>
2023-06-27 12:41:12 -0700

    created 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints alternating characters of a string
+ * @str: pointer to array to be used
+ */
+
+void puts2(char *str)
+{
+	int counter1 = strlen(str);
+	int even = 0;
+
+	while (even <= counter1)
+	{
+		_putchar(str[even]);
+		even = even + 2;
+	}
+	_putchar('\n');
+
+}
