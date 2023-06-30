67052497997261983de2c8b1d98fd5bdd40aac49
stringbitalx <stringbitalx@gmail.com>
2023-06-30 14:14:36 -0700

    created 5-main.c and 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @n: pointer to string to be used
+ * Return: n 
+ */
+
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] -= 32;
+		}
+		i++;
+	}
+	return(n);
+}
