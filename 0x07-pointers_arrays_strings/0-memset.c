43fa3263cb28dde9d171f04868a878da8a294e6a
stringbitalx <stringbitalx@gmail.com>
2023-07-03 05:35:29 -0700

    fnal updates

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - fillls memory with a constant byte type 
- * @s: pointer to character array to be filled 
- * @b: character used to fill 
- * @: first bytes of the character array pointed
+ * _memset - fills memory with a constant byte type
+ * @s: pointer to character array to be filled
+ * @b: character used to fill
+ * @n: first bytes of the character array pointed
+ * Return: A pointer to the memory areas
  */
 
 char *_memset(char *s, char b, unsigned int n)
