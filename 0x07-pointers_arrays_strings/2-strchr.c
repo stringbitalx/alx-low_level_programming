71170d3234f70e02d1b92bcbb8d4bb5d03001315
stringbitalx <stringbitalx@gmail.com>
2023-07-03 12:05:24 -0700

    created 2-main.c and 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to string to be used
+ * @c: character to be located in string
+ * Return: Pointer to 1st occurrence of char in string 0
+ *		otherwise NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int size, i;
+	char *ptr;
+
+	size = strlen(s);
+	for (i = 0; i < size; i++)
+	{
+		s++;
+		ptr = s;
+
+		if (s[i] == c)
+			return (ptr);
+	}
+	return (NULL);
+}
