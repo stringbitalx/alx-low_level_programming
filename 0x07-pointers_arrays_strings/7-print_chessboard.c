8efe4321984e92c6aee36fe8ab5d0cf4e93536bb
stringbitalx <stringbitalx@gmail.com>
2023-07-04 12:19:43 -0700

    final updates

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -23,14 +23,11 @@ int _putchar(char c)
 void print_chessboard(char (*a)[8])
 {
 	int r, c;
-	
-	for (r = 0; r <= 7; r++)
+
+	for (r = 0; r < 8; r++)
 	{
-		for  (c = 0; c <= 7; c++)
-		{
+		for  (c = 0; c < 8; c++)
 			_putchar(a[r][c]);
-		}
 		_putchar('\n');
 	}
-	return;
 }
