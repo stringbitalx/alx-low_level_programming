703a1207daa23b1e6a86516cf68fc22d7eaffce2
stringbitalx <stringbitalx@gmail.com>
2023-07-06 02:15:02 -0700

    created 2-main.c and 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: pointer to string to be used
+ * Return: size
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+	if (*s)
+	{
+		n++;
+		n += _strlen_recursion(s + 1);
+		
+	}
+	return (n);
+}
