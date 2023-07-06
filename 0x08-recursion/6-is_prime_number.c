72a88da3230485018772e5ce30197d3b59052877
stringbitalx <stringbitalx@gmail.com>
2023-07-06 09:23:05 -0700

    final updates

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -18,13 +18,14 @@ int is_prime_number(int n)
  * actual_prime - checks if a number is prime recursively
  * @n: number to evaluate
  * @i: counter variable
+ * Return: 1 if n is prime, 0 if not
  */
 
 int actual_prime(int n, int i)
 {
-	if ( i == 1)
+	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 1)
 		return (0);
-	return (actual_prime (n, i-1));
+	return (actual_prime(n, i - 1));
 }
