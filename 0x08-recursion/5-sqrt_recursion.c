91787450299c47a8ee3f81c7ba788fa22c963e04
stringbitalx <stringbitalx@gmail.com>
2023-07-06 08:58:30 -0700

    created 6-main.c and 6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be used
+ * Return: square root of n
+ */
+
+int _sqrt_recurion(int n)
+{
+	if (n < 0)
+		return(-1);
+	else if (n == 0)
+		return (1);
+	return(
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int actual_prime(int n, int i);
+
+/**
+ * _is_prime_number - checks if input integer is a prime number
+ * @n: input integer
+ * Return: 1 (Success)
+ */
+int _is_prime_number(int n);
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - checks if a number is prime recursively
+ * @n: number to evaluate
+ * @i: counter variable
+ */
+
+int actual_prime(int n, int i);
+{
+	if ( i == 1)
+		return (1);
+	if (n % 1 == 0 && i > 1)
+		return (0)
+	return (actual_prime (n, i-1));
+}
