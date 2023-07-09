cb3fae8d9416387621096ac7832f8bff144be8b9
stringbitalx <stringbitalx@gmail.com>
2023-07-09 05:25:55 -0700

    created 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 Always (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
