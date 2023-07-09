8fc559d9056f08e7959f1c7cc05e3c60bb436fa2
stringbitalx <stringbitalx@gmail.com>
2023-07-09 05:28:35 -0700

    final updates

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,10 +12,7 @@ int main(int argc, char **argv)
 {
 	int i;
 
-	if (argc > 1)
-	{
-		for (i = 0; i < argc; i++)
-			printf("%s\n", argv[i]);
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
