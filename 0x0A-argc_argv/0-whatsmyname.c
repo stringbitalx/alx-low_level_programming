4dbdb060e4b964b0b5d3e8b35133459c176a338e
stringbitalx <stringbitalx@gmail.com>
2023-07-09 05:05:34 -0700

    final updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints program name
@@ -7,7 +8,7 @@
  * Return: 0 Always (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", *argv);
 	return (0);
