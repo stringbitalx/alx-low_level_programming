7b8741a8971dc04d07cbaa692274318dc7d232c9
stringbitalx <stringbitalx@gmail.com>
2023-07-09 04:50:45 -0700

    created 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * argc - argument counter
+ * argv[] - argument vector
+ * Return: 0 Always (Success)
+ */
+
+int main (int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
