c7de4ba31314d96bb7bc042e13a5e110872a2523
stringbitalx <stringbitalx@gmail.com>
2023-07-11 10:39:24 -0700

    created 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates a 2D integer array
+ * @width: number of rows
+ * @height: number of columns
+ * Return: pointer to 2D array otherwise NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int r, c;
+
+	if (width < 0 ||
+		width == 0 ||
+		height < 0 ||
+		height == 0)
+		return (NULL);
+	array = malloc(sizeof(int *) * height);
+	for (c = 0; c < height; c++)
+		array[c] = malloc(sizeof(int) * width);
+	c = 0;
+
+	while (c < height)
+	{
+		for (r = 0; r < width; r++)
+		{
+			array[c][r] = 0;
+		}
+		c++;
+	}
+	return (array);
+}
