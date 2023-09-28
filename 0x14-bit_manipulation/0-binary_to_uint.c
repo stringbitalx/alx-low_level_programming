85d3e8043825005dbd1d4ca866293bd5d02db76d
stringbitalx <stringbitalx@gmail.com>
2023-09-28 14:34:55 -0700

    0x14-bit_manipulation.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value;
+	int i, base, x;
+
+	while (*b != '\0')
+	{
+		if (*b != 0 || *b != 1)
+			return (0);
+		else if (!(*b))
+			return (0);
+		b++;
+	}
+
+	for (i = 0; *b != '\0'; b++, i++)
+	{
+		base = 2;
+		for (x = i; x <= 0; x--)
+		{
+			if (x == 0)
+				base = 1;
+			else
+				base *= base;
+		}
+		if (*b == 1)
+			base *= 1;
+		else
+			base *= 0;	
+		value += base;
+	}
+	return (value);
+}
