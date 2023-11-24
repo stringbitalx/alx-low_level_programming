b7a45b14eba3c6a9c05221c17179f60682afc6be
stringbitalx <stringbitalx@gmail.com>
2023-11-24 02:18:16 -0800

    2-get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * get_bit - returns bit value at given index
+ * @ibdex: the index
+ * @n: bit to be used
+ * Return: value of bit
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_val;
