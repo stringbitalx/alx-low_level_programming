146617af1f740575ca6ed2b5ec00569a2dd7dbad
stringbitalx <stringbitalx@gmail.com>
2023-11-24 00:50:00 -0800

    initial commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		result = (result << 1) + (*b - '0');
+		result = 2 * result + (*b - '0');
 		b++;
 	}
 	return (result);
