4b751a87400b0c53d1904fba62104bdbad43cf6c
stringbitalx <stringbitalx@gmail.com>
2023-11-27 09:23:09 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,22 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
-	int fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		ssize_t w = write(fd, text_content, strlen(text_content));
+		for (; text_content[len];)
+			len++;
+	w = write(fd, text_content, len);
 
-		if (w == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (-1);
