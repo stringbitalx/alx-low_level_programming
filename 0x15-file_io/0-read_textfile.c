2ef3d40988d9c719ed0d09fbd5d71e6876e94be6
stringbitalx <stringbitalx@gmail.com>
2023-11-27 08:10:58 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads text file and prints to stdout
+ * @filename: text file to be read
+ * @letters: number of letters to be read
+ * Return: number of letters read
+ * 0 if file cannot be opened, read, is NULL
+ * or doesn't write specified number of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, w, t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1);
+	if (buf == NULL)
+		return (0);
+	t = read(fd, buf, letters);
+	buf[t] = '\0';
+
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
