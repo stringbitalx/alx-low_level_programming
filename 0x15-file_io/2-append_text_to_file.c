1082b26e86231a83dcb28408554b14c0fd23071c
stringbitalx <stringbitalx@gmail.com>
2023-11-28 10:32:15 -0800

    2-append_text_to_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at EOF
+ * @filename: pointer to file name
+ * @text_content: string to add to EOF
+ * Return: 1 on succes -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		for (; text_content[len];)
+			len++;
+	o = open(filename, O_WRONLY | O_APPEND);
+	if (o == -1)
+		return (-1);
+
+	w = write(o, Text_content, len);
+	if (w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
