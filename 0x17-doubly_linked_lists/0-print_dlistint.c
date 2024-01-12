0f055024dd1eb81859b556485f4b52e46e056429
stringbitalx <stringbitalx@gmail.com>
2024-01-12 04:14:23 -0800

    final updates

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	int count;
+
+	count = 0;
 
 	if (h == NULL)
 		return (count);
+
 	while (h->prev != NULL)
 		h = h->prev;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
