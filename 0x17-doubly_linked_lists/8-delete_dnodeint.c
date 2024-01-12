8dbd74c06c4b2ed58366fc427487728745e2c4e2
stringbitalx <stringbitalx@gmail.com>
2024-01-12 06:17:59 -0800

    final updates

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (i == 0)
 			{
 				*head = h1->next;
-				if (*h != NULL)
+				if (*head != NULL)
 					(*head)->prev = NULL;
 			}
 			else
