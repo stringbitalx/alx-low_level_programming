f02912ec52794b334b4429eb1cf023acb549b91c
stringbitalx <stringbitalx@gmail.com>
2024-01-12 05:50:29 -0800

    final updates

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the summ of data in linked list
+ * @head: head of the list
+ * Return: the sum of the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head-.next;
+		}
+	}
+
+	return (sum);
+}
