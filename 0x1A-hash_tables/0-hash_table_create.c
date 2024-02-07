1d0cee245de6f96bf002acd5a5abbc9707bd40b4
stringbitalx <stringbitalx@gmail.com>
2024-02-07 13:07:41 -0800

    initial commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ * Return: pointer to the created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	hash_table_t *new_ht;
+	unsigned long int i = 0;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (size == 0)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	new_ht = malloc(sizeof(hash_table_t));
+	if (!new_ht)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
 
-	return (ht);
+	new_ht->size = size;
+	new_ht->array = calloc(new_ht->size, sizeof(hash_table_t *));
+	if (!new_ht->array)
+		return (NULL);
+	for (i = 0; i < new_ht->size; i++)
+	{
+		new_ht->array[i] = NULL;
+	}
+	return (new_ht);
 }
